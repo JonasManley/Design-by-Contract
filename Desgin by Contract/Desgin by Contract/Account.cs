3b331b6f331da539aafd37f8a8bbe08ae6923f2f
JonasManley <54889189+JonasManley@users.noreply.github.com>
2019-10-15 15:48:10 +0200

    Project added

---
+++ Desgin by Contract/Desgin by Contract/Account.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.Contracts;
+
+namespace Desgin_by_Contract
+{
+    class Account
+    {
+        public double _balance =  500;
+
+        public Account()
+        {
+           
+        }
+
+
+        public double Deposit(double amount)
+        {
+            var value = amount;
+            Contract.Requires(amount > 0, "amount is less then/or 0");
+            Contract.Ensures(Contract.Result<double>() >= 0);
+            Contract.EnsuresOnThrow<ArgumentException>(Contract.OldValue<double>(value) == amount);
+
+            var NewBalance =_balance + amount;
+
+            return NewBalance;
+        }
+
+        public double Withdraw(double amount)
+        {
+            var value = amount;
+            Contract.Requires(amount > 0, "amount is less then/or 0");
+            Contract.Requires(_balance > amount);     
+            Contract.Ensures(Contract.Result<double>() >= 0);
+            Contract.EnsuresOnThrow<ArgumentException>(Contract.OldValue<double>(value) == amount);
+
+            var NewBalance = _balance - amount;
+
+            return NewBalance;
+        }
+    }
+}
---
+++ Desgin by Contract/Desgin by Contract/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desgin_by_Contract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acccount = new Account();
+
+            //Example: Going bad
+            //var deposit = acccount.Deposit(0);
+            //Console.WriteLine("Result =" + deposit);
+
+            //Example: Going good
+            //var deposit = acccount.Deposit(200);
+            //Console.WriteLine("Result =" + deposit);
+
+            //Example: Going bad
+            //var withdrawbad = acccount.Withdraw(700);
+
+            //Example: Going good
+            //var withdrawGood = acccount.Withdraw(242);
+            //Console.WriteLine(withdrawGood);
+
+            Console.ReadLine();
+        }
+    }
+}
